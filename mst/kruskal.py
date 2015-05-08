3361103082e6f27ea57db66b0fe360ce66006491
Pavel Shtykovskiy <pavel.sht@yandex.ru>
2015-05-08 11:09:21 +0300

    MST funcs stub

---
+++ mst/kruskal.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+
+import sys
+from operator import attrgetter
+
+
+class Edge(object):
+    def __init__(self, tail=None, head=None, weight=None):
+        self.head = head
+        self.tail = tail
+        self.weight = weight
+
+
+class Graph(object):
+    def __init__(self):
+        self.adj_list = {}
+    def add_edge(self, tail, head, weight):
+        self.adj_list[tail].append(Edge(tail, head, weight))
+        self.adj_list[head].append(Edge(head, tail, weight))
+    def vertices(self):
+        return self.adj_list.iteritems()
+    def edges(self, v):
+        return self.adj_list[v]
+
+def kruskal(graph):
+    
+    edges = sorted([edge for v, edge in graph.vertices()], key=attrgetter('weight'))
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    pass
